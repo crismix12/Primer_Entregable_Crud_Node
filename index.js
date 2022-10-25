be3187883d8d3b9d161488d63ccabcfe67845914
crismix12 <91161880+crismix12@users.noreply.github.com>
2022-10-25 02:30:32 -0400

    deletedcomments

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-//importamos modulos que usaremos http, path, fs
 const http = require('http');
 const path = require('path');
 const fs = require('fs/promises');
@@ -6,42 +5,22 @@ const fs = require('fs/promises');
 const PORT = 8000;
 
 const app = http.createServer(async (request, response) =>{
-    // dentro del objeto request podemos leer el metodo de la peticion
-    // GET -- POST -- PUT -- DELETE
     const requestMethod = request.method;
     const requestURL = request.url;
-    // leer la ruta de la peticion y el metodo
-    // console.log(requestURL, requestMethod);
-    
-    //responder el data.json cuando se realice un get al endpoiont /apiv1/tasks
     if(requestURL === '/apiv1/tasks'){
-        const jsonPath = path.resolve('./data.json')
-        const jsonFile = await fs.readFile(jsonPath, 'utf-8') //acepta dos parametros "ruta y codificacion"
+        const jsonPath = path.resolve('./data.json');
+        const jsonFile = await fs.readFile(jsonPath, 'utf-8');
         if(requestMethod === 'GET'){
-            //respondo con data.json
-            //obtener la ruta del data.json
-            // console.log(jsonPath);
             response.setHeader("Content-Type", "application/json");
             response.writeHead('200');
             response.write(jsonFile);
-            // response.end();
         }
         if(requestMethod === 'POST'){
             request.on('data', async (data) => {
-                //recibo un JSON
-                // necesito agregar la data a data.json
-                // necesito obtener la informacion *
-                // necesito escribir el archivo
-                    //primero leer el archivo *
-                        //necesito la ruta del archivo  *
-                    //escribir en el archivo
                 const newTask = JSON.parse(data);
-                // console.log(parsed);
-                // const dataPath = path.resolve('./data.json');
                 const arr = JSON.parse(jsonFile);
                 arr.push({...newTask, id: getLastId(arr)});
                 await fs.writeFile(jsonPath, JSON.stringify(arr));
-                // console.log(arr);
             })
             response.setHeader("Content-Type", "application/json");
             response.writeHead('201');
@@ -76,18 +55,8 @@ const app = http.createServer(async (request, response) =>{
            response.writeHead('200');           
         }
     } else {
-        response.writeHead("503") //poner un estado de la respuesta
+        response.writeHead("503");
     }
-    
-    //ya que saben como leer la ruta de la peticion
-    // el metodo de la peticion
-    // obtener la informacion que se envia por el body
-
-    //1- cuando se haga un post agregar un elemento al json y responder con un status 201
-    //2- cuando se haga un PUT (solamente se enviara por el body {status: true}) se envia un id: data y 
-    //   actualizar el elemento y responder con un status investigar con que estado puedes responder
-    //3- CUando se haga un delete eliminar el elemento del json y responder (investigar con que status code puedes responder)
-    
     response.end();
 });
 
